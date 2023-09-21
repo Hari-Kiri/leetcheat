bf15344336a47f87683b4fe8b800f19f6c9b4d52
Hari-Kiri <pratamahari89@gmail.com>
2023-09-22 02:12:42 +0700

    Create contains duplicate leetcode solution.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Hari-Kiri/leetcheat
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Hari-Kiri/leetcheat/ContainsDuplicate"
+)
+
+func main() {
+	fmt.Println(ContainsDuplicate.Run([]int{2, 14, 18, 22, 22}))
+}
