46dd16961ccc3098fc0529f2191e38a42cd2c3f9
Hari-Kiri <pratamahari89@gmail.com>
2023-09-22 02:15:00 +0700

    Change Run to Solution.

---
+++ ContainsDuplicate/Solution.go
@@ -0,0 +1,28 @@
+package ContainsDuplicate
+
+// Check if any duplicate in array.
+func Solution(nums []int) bool {
+	// Declare return value variable
+	var anyDuplicate bool
+
+	// Declare data map or other key = value pair data in other language
+	temp := make(map[int]bool, 0)
+
+	// Loop through nums
+	for i := 0; i < len(nums); i++ {
+
+		// Check if any key named with nums[i] with value true.
+		// If yes set return value to true then break the loop
+		if temp[nums[i]] {
+			anyDuplicate = true
+			break
+		}
+
+		// If not any key named with nums[i] with value true,
+		// set it for next loop
+		temp[nums[i]] = true
+
+	}
+
+	return anyDuplicate
+}
