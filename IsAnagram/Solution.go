7cbb329b829608a86a157477f9f0e23595df0020
Hari-Kiri <pratamahari89@gmail.com>
2023-09-22 14:40:49 +0700

    Change solution.

--- IsAnagram/Solution.go
+++ IsAnagram/Solution.go
@@ -1,7 +1,7 @@
 package IsAnagram
 
 import (
-	"strings"
+	"fmt"
 )
 
 // Check two string is anagram or not.
@@ -13,29 +13,12 @@ func Solution(s string, t string) bool {
 		return false
 	}
 
-	// Lower the string
-	sLower := strings.ToLower(s)
-	tLower := strings.ToLower(t)
-	// Create data map of integer or any othe key = value pair data type in other language
-	temp := make(map[string]int)
-	// Loop through sLower
-	for i := 0; i < len(sLower); i++ {
-		// Add new key to temp using sLower[i] as key and increment value
-		temp[string(sLower[i])] = temp[string(sLower[i])] + 1
+	temp0 := make(map[string]int)
+	temp1 := make(map[string]int)
+	for i := 0; i < len(s); i++ {
+		temp0[string(s[i])] = temp0[string(s[i])] + 1
+		temp1[string(t[i])] = temp1[string(t[i])] + 1
 	}
 
-	// Loop through tLower
-	for i := 0; i < len(tLower); i++ {
-		// Decrease the value in every key.
-		// We use tLower[i] as key now and when the key from tLower[i] not present in temp,
-		// The compiler will add new key with value minus 1 (-1).
-		temp[string(tLower[i])] = temp[string(tLower[i])] - 1
-		// Detect -1 then return false
-		// When minus 1 detected s & t is not anagram
-		if temp[string(tLower[i])] < 0 {
-			return false
-		}
-	}
-
-	return true
+	return fmt.Sprint(temp0) == fmt.Sprint(temp1)
 }
