3d70b8ec785368182f454b108087456ced468da1
Hari-Kiri <pratamahari89@gmail.com>
2023-09-22 23:30:13 +0700

    Add benchmark.

--- IsAnagram/Solution_test.go
+++ IsAnagram/Solution_test.go
@@ -1,13 +1,40 @@
 package IsAnagram
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type solutionTest struct {
+	s, t     string
+	expected bool
+}
+
+var solutionTests = []solutionTest{
+	{"anagram", "nagaram", true},
+	{"rat", "car", false},
+	{"ac", "bb", false},
+	{"aacc", "ccac", false},
+	{"caca", "ccaa", true},
+	{"qwertyuiopasdfghjklzxcvbnm", "lkjhgfdsaqwertyuiopmnbvcxz", true},
+	{"poiuqwertyhjklgfdsabnmvcxz", "qazwsxedcrfvtgbyhnujmikol", false},
+}
 
 func TestSolution(t *testing.T) {
-
-	got := Solution("anagram", "nagaram")
-	want := true
-
-	if got != want {
-		t.Errorf("got %t, wanted %t", got, want)
+	for i := 0; i < len(solutionTests); i++ {
+		if output := Solution(solutionTests[i].s, solutionTests[i].t); output != solutionTests[i].expected {
+			t.Errorf("Output %t not equal to expected %t", output, solutionTests[i].expected)
+		}
 	}
+}
+
+func BenchmarkAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Solution("mnbvcxzlkjhgfdsaqwertyuiop", "ghjklfdsapqowieurytbvncmxz")
+	}
+}
+
+func ExampleSolution() {
+	fmt.Println(Solution("polisi", "isilop"))
+	// Output: true
 }
