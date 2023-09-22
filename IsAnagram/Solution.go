7ef8afc99efd337aa19db01bfda0a19a6d9cbc2e
Hari-Kiri <pratamahari89@gmail.com>
2023-09-22 23:29:53 +0700

    Change solution logic.

--- IsAnagram/Solution.go
+++ IsAnagram/Solution.go
@@ -1,24 +1,45 @@
 package IsAnagram
 
-import (
-	"fmt"
-)
-
 // Check two string is anagram or not.
-// The goal is to check count every alphabet in s & t
-// then compare it has same count or not.
+// The goal is to count up every letter in s
+// then count it down with t. If there is a
+// letter in t which is not in s the count down
+// will set it as -1, so that is we detect not
+// anagram.
 func Solution(s string, t string) bool {
-	// Stop when s & t not in same length
+	// Stop when s & t not in same length.
+	// Length not same is absolutely not
+	// anagram.
 	if len(s) != len(t) {
 		return false
 	}
 
-	temp0 := make(map[string]int)
-	temp1 := make(map[string]int)
+	// Create a temporary array variable
+	// with length of 26, which is representing
+	// 26 letters of alphabet
+	temp := make([]int, 26)
+	// Loop through s. For loop in go will
+	// convert the string value to byte.
+	// We will reduce the byte value each letter in s
+	// with 97. FYI 97 is byte of small letter 'a'.
+	// a = 97, b = 98, c = 99 ...
 	for i := 0; i < len(s); i++ {
-		temp0[string(s[i])] = temp0[string(s[i])] + 1
-		temp1[string(t[i])] = temp1[string(t[i])] + 1
+		// Byte of the s[i] value - 97 will get
+		// the index of temporary array and
+		// increment by 1 the value in that index
+		index := int(s[i] - 97)
+		temp[index] += 1
+	}
+	// Loop through t and count down every alphabet count of s
+	// inside temporary slice variable.
+	for i := 0; i < len(t); i++ {
+		index := int(t[i] - 97)
+		temp[index] -= 1
+		// detect the -1 value then we get it not anagram
+		if temp[index] < 0 {
+			return false
+		}
 	}
 
-	return fmt.Sprint(temp0) == fmt.Sprint(temp1)
+	return true
 }
