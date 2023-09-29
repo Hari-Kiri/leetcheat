565ff51ce45e1a6ad70b0491f34e1013ae3dcd55
Hari-Kiri <pratamahari89@gmail.com>
2023-09-29 12:46:24 +0700

    Add test command for two sum solution.

--- run-test.sh
+++ run-test.sh
@@ -1,2 +1,3 @@
-go test ./ContainsDuplicate -v -bench=Add
-go test ./IsAnagram -v -bench=Add
+go test ./ContainsDuplicate -v -bench=Add -benchmem
+go test ./IsAnagram -v -bench=Add -benchmem
+go test ./TwoSum -v -bench=Add -benchmem
