0e67153202ed1f6a893cd23ed00952a928ad0da8
Hari-Kiri <pratamahari89@gmail.com>
2023-09-29 12:46:57 +0700

    Add two sum solution.

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/Hari-Kiri/leetcheat/ContainsDuplicate"
 	"github.com/Hari-Kiri/leetcheat/IsAnagram"
+	"github.com/Hari-Kiri/leetcheat/TwoSum"
 )
 
 func main() {
 	fmt.Println(ContainsDuplicate.Solution([]int{22, 14, 2, 18, 22}))
 	fmt.Println(IsAnagram.Solution("anagram", "nagaram"))
+	fmt.Println(TwoSum.Solution([]int{2, 7, 11, 15}, 18))
 }
