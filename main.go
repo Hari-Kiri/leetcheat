c806e22b6bbf28276cab4f6add36f3d1518e9cd1
Hari-Kiri <pratamahari89@gmail.com>
2023-09-30 15:37:50 +0700

    Add ConcatenationOfArray.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/Hari-Kiri/leetcheat/ConcatenationOfArray"
 	"github.com/Hari-Kiri/leetcheat/ContainsDuplicate"
 	"github.com/Hari-Kiri/leetcheat/IsAnagram"
 	"github.com/Hari-Kiri/leetcheat/TwoSum"
@@ -12,4 +13,5 @@ func main() {
 	fmt.Println(ContainsDuplicate.Solution([]int{22, 14, 2, 18, 22}))
 	fmt.Println(IsAnagram.Solution("anagram", "nagaram"))
 	fmt.Println(TwoSum.Solution([]int{2, 7, 11, 15}, 18))
+	fmt.Println(ConcatenationOfArray.Solution([]int{1, 2, 1}))
 }
--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,4 @@
 go test ./ContainsDuplicate -v -bench=Add -benchmem
 go test ./IsAnagram -v -bench=Add -benchmem
 go test ./TwoSum -v -bench=Add -benchmem
+go test ./ConcatenationOfArray -v -bench=Add -benchmem
