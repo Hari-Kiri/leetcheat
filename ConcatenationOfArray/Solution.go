2ae825d049534db0b55e6ef9a05bc7fc9778a612
Hari-Kiri <pratamahari89@gmail.com>
2023-09-30 15:40:22 +0700

    Add comment.

--- ConcatenationOfArray/Solution.go
+++ ConcatenationOfArray/Solution.go
@@ -1,5 +1,9 @@
 package ConcatenationOfArray
 
+// Given an integer array nums of length n, you want to create an array ans of length 2n
+// where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+// Specifically, ans is the concatenation of two nums arrays. The solution is simple,
+// just append nums itself
 func Solution(nums []int) []int {
 	return append(nums, nums...)
 }
